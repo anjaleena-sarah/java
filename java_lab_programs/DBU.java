efe4102e1cd977e4fc5108d0d6257bcca7f3bc2a
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-03-25 22:14:49 +0530

    Add files via upload

---
+++ java_lab_programs/DBU.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+class DBU {
+
+   public static void main(String args[]) {
+	Connection con;
+	Statement st;
+	int no;
+	String name;
+	float sal;
+	Scanner s = new Scanner(System.in);
+	try {
+	  Class.forName("com.mysql.jdbc.Driver");
+	  con = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp_db?characterEncoding=utf8","root","");
+	  st = con.createStatement();
+	  System.out.println("Enter no and new name and salary");
+	  no = s.nextInt();
+	  s.nextLine();
+	  name = s.nextLine();
+	  sal = s.nextFloat();
+	  String str = "update emp set empname='";
+	  str = str + name + "',esal=";
+	  str = str + sal +" where eid=" + no;
+	  System.out.println(str);
+	  st.executeUpdate(str);
+	  System.out.println("One Record is Updated..");
+ 	}
+	catch(Exception e) {
+		System.out.println("Error " + e);
+	}
+  }
+}
