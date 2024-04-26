44ae82c2d6f0fca30bcfec4c222fb6764ba8818e
ANJALEENA SARAH K R <140636934+anjaleena-sarah@users.noreply.github.com>
2024-04-26 14:38:44 +0530

    Calculator.java

--- java_lab_programs/Calculator.java
+++ java_lab_programs/Calculator.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent; 
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener; 
 
 public class Calculator extends JFrame implements ActionListener {
     private JTextField textField;
